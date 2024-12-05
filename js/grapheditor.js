72da02b2b4d1b6422f50b720f1a54673b452d471
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-12-05 21:00:54 +0530

    can remove points and segments, click add points

---
+++ js/grapheditor.js
@@ -0,0 +1,26 @@
+class GraphEditor {
+    constructor(canvas, graph) {
+        this.canvas = canvas;
+        this.graph = graph;
+        this.selected = null;
+        this.ctx = this.canvas.getContext("2d");
+
+        this.#addEventListeners();
+    }
+
+    #addEventListeners() {
+        this.canvas.addEventListener("mousedown", (evt) => {
+            const mouse = new Point(evt.offsetX, evt.offsetY);
+            this.graph.addPoint(mouse);
+            this.selected = mouse;
+        });
+    };
+
+    display() {
+        this.graph.draw(this.ctx);
+
+        if (this.selected) {
+            this.selected.draw(this.ctx, { outline: true});
+        }
+    }
+}
--- js/math/graph.js
+++ js/math/graph.js
@@ -25,6 +25,35 @@ class Graph{
         this.segments.push(segment);
     }
 
+    tryAddSegment(segment){
+        if (!this.containsSegment(segment) && !segment.p1.equals(segment.p2)){
+            this.addSegment(segment);
+            return true;
+        }
+
+        return false;
+    }
+
+    containsSegment(segment){
+        return this.segments.find((s) => s.equals(segment));
+    }
+
+    removeSegment(segment){
+        this.segments.splice(this.segments.indexOf(segment), 1);
+    }
+
+    removePoint(point){
+        this.points.splice(this.points.indexOf(point), 1);
+        this.segments = this.segments.filter((seg) => !seg.includes(point));
+    }
+
+    dispose() {
+        this.points.length = 0;
+        this.segments.length = 0;
+        this.points = null;
+        this.segments = null;
+    }
+
     draw(ctx){
         for (const seg of this.segments) {
             seg.draw(ctx);
